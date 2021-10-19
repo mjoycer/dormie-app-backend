c0e3acc2becde19c702586209327542f27749aa4
rmjm <recto_mary@yahoo.com>
2021-10-19 23:43:48 +0800

    fix: fix cors error

--- index.js
+++ index.js
@@ -7,7 +7,13 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 app.use(bodyParser.json());
-app.use(cors());
+
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials: true,            //access-control-allow-credentials:true
+    optionSuccessStatus: 200
+}
+app.use(cors(corsOptions));
 
 mongoose.connect('mongodb+srv://uplift_joyce2021:uplift2021@cluster0.3amhf.mongodb.net/mjoycer-db-001?retryWrites=true&w=majority',
     {
