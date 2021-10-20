c3c47ae7eded556a76a394236177c7c8aedb000e
rmjm <recto_mary@yahoo.com>
2021-10-20 17:13:38 +0800

    fix: resolve cors issue

--- routes/users.js
+++ routes/users.js
@@ -11,8 +11,8 @@ router.get('/', auth.verify, (req, res) => {
     });
 });
 
-router.post('/email-exists', (req, res) => {
-    Users.findOne({ email: req.body.email }).then(data => {
+router.post('/email-exists', async (req, res) => {
+    await Users.findOne({ email: req.body.email }).then(data => {
         if (data) {
             res.send(true);
         } else {
@@ -22,42 +22,33 @@ router.post('/email-exists', (req, res) => {
 });
 
 router.post('/register', async (req, res) => {
-    try {
-        let hashedPwd = await bcrypt.hash(req.body.password, 10);
-        let user = {
-            name: req.body.name,
-            email: req.body.email,
-            password: hashedPwd
-        }
+    let hashedPwd = await bcrypt.hash(req.body.password, 10);
 
-        let newUser = new Users(user);
-        newUser.save().then(data => {
-            res.send('User has been created.');
-        });
-    } catch {
-        console.log(err);
+    let user = {
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPwd
     }
 
+    let newUser = new Users(user);
+    newUser.save().then(data => {
+        res.send('User has been created.');
+    });
 });
 
 router.post('/login', async (req, res) => {
-    try {
+    let user = await Users.findOne({ email: req.body.email });
 
-        let user = await Users.findOne({ email: req.body.email });
-
-        if (user) {
-            let match = await bcrypt.compare(req.body.password, user.password);
-            if (match) {
-                res.send({ auth: auth.createAccessToken(user) });
-                console.log('Login succesful');
-            } else {
-                res.send({ error: 'Invalid Login' });
-            }
+    if (user) {
+        let match = await bcrypt.compare(req.body.password, user.password);
+        if (match) {
+            res.send({ auth: auth.createAccessToken(user) });
+            console.log('Login succesful');
         } else {
-            res.send({ error: 'Email not found' });
+            res.send({ error: 'Invalid Login' });
         }
-    } catch {
-        console.log(err);
+    } else {
+        res.send({ error: 'Email not found' });
     }
 });
 
