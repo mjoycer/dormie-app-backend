69baa7d52822867d78f2e18d4e50be5b18c7e069
Mary Joyce <recto_mary@yahoo.com>
2024-03-12 23:33:19 +0800

    [FIX]: replace login endpoint functions with passport authentication

--- src/routes/auth.mjs
+++ src/routes/auth.mjs
@@ -1,25 +1,27 @@
+import passport from "passport";
 import { Users } from "../mongoose/schema/users.mjs";
 import { Router } from "express";
 
 const router = Router();
 
-router.post('/login', async (req, res) => {
-    let user = await Users.findOne({ email: req.body.email });
+router.post('/login', passport.authenticate('local'), async (req, res) => {
+    res.sendStatus(200);
+    // let user = await Users.findOne({ email: req.body.email });
     
-    if (user) {
-        let match = await bcrypt.compare(req.body.password, user.password);
-        if (match) {
-            const auth = createAccessToken(user);
-            req.session.user = user;
-            console.log('Login succesful');
-            console.log(req.session.user);
-            return res.send({ auth: auth, user: user });
-        } else {
-            return res.status(401).send({ error: 'Invalid Login' });
-        }
-    } else {
-       return res.send({ error: 'Email not found' });
-    }
+    // if (user) {
+    //     let match = await bcrypt.compare(req.body.password, user.password);
+    //     if (match) {
+    //         const auth = createAccessToken(user);
+    //         req.session.user = user;
+    //         console.log('Login succesful');
+    //         console.log(req.session.user);
+    //         return res.send({ auth: auth, user: user });
+    //     } else {
+    //         return res.status(401).send({ error: 'Invalid Login' });
+    //     }
+    // } else {
+    //    return res.send({ error: 'Email not found' });
+    // }
 });
 
 router.get('/status', (req, res) => {
--- src/strategies/local-strategy.mjs
+++ src/strategies/local-strategy.mjs
@@ -1,6 +1,7 @@
 import passport from "passport";
 import { Strategy } from 'passport-local';
 import { Users } from '../mongoose/schema/users.mjs';
+import bcrypt from 'bcrypt';
 
 passport.serializeUser((user, done) => {
   done(null, user.id)
@@ -18,16 +19,17 @@ passport.deserializeUser((id, done) => {
 
 
 export default passport.use(
-  new Strategy({usernameField: "email"}, (username, password, done) => {
+  new Strategy({usernameField: "email"}, async (username, password, done) => {
+    
     try {
-      const findUser = Users.findOne(username);
+      const findUser = await Users.findOne({email: username});
       if(!findUser) throw new Error('User not found');
 
-      let match = bcrypt.compare(password, findUser.password);
+      let match = bcrypt.compareSync(password, findUser.password)
       if (!match) throw new Error('Invalid Credentials');
-
       done(null, findUser)
     } catch(err) {
+
       done(err, null);
     }
   })
